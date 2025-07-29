b8e8c59f330412cc7dac08996fced6dd3321f6c2
Ronaldo Oliveira <101574101+ronaldorosaoliveira@users.noreply.github.com>
2025-07-29 11:52:58 -0300

    Create desafio_xadrez_aventureiro.c
    desafio_xadrez_aventureiro.c

---
+++ desafio_xadrez_aventureiro.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    // Movimentação da TORRE - 5 casas para a direita, usando o comando FOR
+    int movimentotorre = 5;
+    printf("Movimento da Torre:\n");
+    for (int i = 1; i <= movimentotorre; i++) {
+        printf("Direita\n");
+    }
+
+    // Movimentação do BISPO - 5 casas na diagonal para cima e à direita, usando o comando WHILE
+    int movimentobispo = 5, contadorbispo = 0;
+    printf("\nMovimento do Bispo:\n");
+    while (contadorbispo < movimentobispo) {
+        printf("Cima\n");
+        printf("Direira\n");
+        contadorbispo++;
+    }
+
+    // Movimentação da RAINHA - 8 casas para a esquerda usando o comando DO-WHILE
+    int movimentorainha = 8, contadorrainha = 0;
+    printf("\nMovimento da Rainha:\n");
+    do {
+        printf("Esquerda\n");
+        contadorrainha++;
+    } while (contadorrainha < movimentorainha);
+
+    // Movimentação do CAVALO - L (baixo, baixo, esquerda)
+    printf("\nMovimento do Cavalo:\n");
+    int movimentocavalo = 1;
+        while (movimentocavalo--) // movimento em L do cavalo
+        {
+            for (int i = 0; i < 2; i++) {
+                printf("Cima\n"); // Imprime "Cima" duas vezes
+            }
+            printf("Direita\n"); // Imprime "Direita" uma vez
+        }
+        
+
+    
+    return 0;
+
+}
